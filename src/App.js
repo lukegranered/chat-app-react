1b9d046463d9dca39a4671ff04acb7d29b91550a
Luke G <lukejgranered@gmail.com>
2022-07-09 09:49:01 -0500

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <ChatEngine 
+      height='100vh'
+      projectID='d9119d3a-0895-4159-9a72-0c451991978f'
+      userName='Gerges'
+      userSecret='123456'
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
